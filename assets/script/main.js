cd43a7e1ef4694d5afb94a0033e6569519730cb7
DaiMM <dai.mmachado@gmail.com>
2022-09-29 10:31:23 -0300

    melhorando o codigo

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,9 +1,6 @@
-const btnToggle = document.getElementById("btn-toggle");
 const container = document.getElementById("container");
+const checkbok = document.getElementById("checkbox");
 
-btnToggle.onclick = (e) => {
-  e.preventDefault();
-  
-  container.classList.toggle("container__toggle--theme-dark")
-  btnToggle.classList.toggle("toggle__btn--position-rigth")
+checkbok.onclick = () => {  
+  container.classList.toggle("container__switch--theme-dark");
 }
